e2b7fee58a13135c9fb51f8001cf6595924d0bef
Salome-Hernandez-Adolfo-Angel <64882685+Salome-Hernandez-Adolfo-Angel@users.noreply.github.com>
2022-03-26 00:38:53 -0600

    incorporación de usuarios api

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\API\DireccionesController;
 use App\Http\Controllers\API\Expedientes_vendedoresController;
 use App\Http\Controllers\API\ProductosController;
 use App\Http\Controllers\API\VentasController;
+use App\Http\Controllers\Api\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,7 @@ use App\Http\Controllers\API\VentasController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::apiResource('usuarios', UsersController::class)->names('api-usuarios');
 Route::apiResource('ventas', VentasController::class)-> names('api-ventas');
 Route::apiResource('expedientes_vendedores', Expedientes_vendedoresController::class)-> names('api-expedientes_vendedores');
 Route::apiResource('detalles_ventas', Detalles_ventaController::class)->names('api-detalles_venta');
